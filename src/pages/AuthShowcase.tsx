1a1caaeaf55c2838fc20c07c6111f229436312ba
Chaitali Patel <cpatel1@ualberta.ca>
2022-11-01 11:03:21 -0600

    disable send button if no phone numbers checked

--- src/pages/AuthShowcase.tsx
+++ src/pages/AuthShowcase.tsx
@@ -286,8 +286,9 @@ const AuthShowcase: React.FC = () => {
             />
           </div>
           <button
-            className="border-2 border-indigo-400 py-2 px-4 mt-4 rounded-3xl shadow-lg min-w-full"
+            className="border-2 border-indigo-400 py-2 px-4 mt-4 rounded-3xl shadow-lg min-w-full disabled:opacity-50"
             type="submit"
+            disabled={checkedPhoneNumbers.size === 0}
           >
             Send
           </button>
